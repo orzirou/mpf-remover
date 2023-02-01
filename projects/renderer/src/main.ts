a5f959220da045ca57297ce0837937ef9b1da7b4
orzirou <S.ohtawa0707@gmail.com>
2023-02-01 23:28:18 +0900

    production時はconsole.logの出力を無効化

--- projects/renderer/src/main.ts
+++ projects/renderer/src/main.ts
@@ -6,7 +6,10 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+
+  window.console.log = () => {};
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
