076549f05104fb7f3c89c7f05c86990a2bfbba5d
orzirou <S.ohtawa0707@gmail.com>
2023-03-08 20:43:54 +0900

    テーマ見直し

--- projects/renderer/src/app/app.component.ts
+++ projects/renderer/src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements AfterViewInit {
   title = 'MPF Remover';
 
-  ngOnInit() {
-    document.body.classList.add(
-      matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark-theme'
-        : 'light-theme'
-    );
+  ngAfterViewInit() {
+    const darkmodeDOM = document.getElementById('theme-handler') as HTMLElement;
+    const observer = new IntersectionObserver(() => {
+      if (getComputedStyle(darkmodeDOM).display === 'block') {
+        document.body.classList.remove('light-theme');
+        document.body.classList.add('dark-theme');
+      } else {
+        document.body.classList.remove('dark-theme');
+        document.body.classList.add('light-theme');
+      }
+    });
+    observer.observe(darkmodeDOM);
   }
 }
