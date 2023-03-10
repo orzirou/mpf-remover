50dc1168b9450253f98f8a55fa2aa48e8b244361
orzirou <S.ohtawa0707@gmail.com>
2023-03-10 21:24:31 +0900

    Exif表示用Componentの枠作成

---
+++ projects/renderer/src/app/feature/exif-ref/exif-ref.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exif-ref',
+  templateUrl: './exif-ref.component.html',
+  styleUrls: ['./exif-ref.component.scss']
+})
+export class ExifRefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/renderer/src/app/feature/exif-ref/exif-ref.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExifRefComponent } from './exif-ref.component';
+
+
+
+@NgModule({
+  declarations: [
+    ExifRefComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExifRefModule { }
